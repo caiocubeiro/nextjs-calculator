2668a06786f4c9f41e30f7817314a8edf6f3cf2f
Caio Cubeiro <caiocubeiroedu@gmail.com>
2022-11-04 19:04:24 -0300

    keygrid colors

--- components/botão.tsx
+++ components/botão.tsx
@@ -5,11 +5,26 @@ interface BotaoProps {
     tipo?: 'number' | 'operator' | 'clean' | 'result'
 }
 
+let estilo = styles.button
 export default function Botao(props: BotaoProps) {
+
+        if (props.tipo === 'number'){
+            estilo = styles.button
+        }else if(props.tipo === 'operator'){
+            estilo = styles.buttonoperator
+        }
+        else if(props.tipo === 'clean'){
+            estilo = styles.buttonclear
+        }
+        else{
+            estilo = styles.buttonresult
+        }
+
+    
     return (
         <div className={styles.divisoes}>
             <button
-                className={styles.button}
+                className={estilo}
             >{props.valor}</button>
         </div>
     )
